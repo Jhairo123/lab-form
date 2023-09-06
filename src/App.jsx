ffebd8dcadacdc75b6ed751c1b1efe112d594ae2
JUAN DAVID SAAVEDRA REYES <107573346+jsaav11@users.noreply.github.com>
2023-09-05 21:12:59 -0500

    resolve validates

--- src/App.jsx
+++ src/App.jsx
@@ -3,60 +3,99 @@ import "./App.css";
 import { registerUser } from "./services/registerUser";
 import { useEffect } from "react";
 
+
+function getErrorMessageForField(name, value, value2) {
+  if(value === ''){
+    return `${name} is required`;
+  }
+  if(value){
+    switch (name) {
+      case "email":
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        if (!emailRegex.test(value.toString())) {
+          return "email is invalid";
+        } else {
+          return "";
+        }
+      case "age":
+        if (value < 18) {
+          return "you must be above 18 to register";
+        }
+        break;
+      case "password":
+        if (value.length < 6) {
+          return "password is too short";
+        }
+        break;
+      case "passwordCheck":
+        if (value !== value2) {
+          return "passwords do not match";
+        }
+        break;
+      default:
+        return "";
+    }
+  }
+ 
+}
+
 export function App() {
-  const [field, setField] = useState({
-    email: "",
-    name: "",
-    age: "",
-    password: "",
-    passwordCheck: "",
-  });
+  const [field, setField] = useState(undefined);
+
   const [error, setError] = useState({
-    email: "",
-    name: "",
-    age: "",
-    password: "",
-    passwordCheck: "",
+    email: undefined,
+    name: undefined,
+    age: undefined,
+    password: undefined,
+    passwordCheck: undefined,
   });
   const [enableButton, setEnableButton] = useState(true);
-  // const onSubmit = (data) => {
-  //   registerUser(data);
-  // };
 
-  function handleOnSubmit(e) {
-    e.preventDefault();
-    if (field.email == "") alert("email is required");
-  }
+  const onSubmit = (data) => {
+    registerUser(data);
+  };
+
+ 
   function handleOnChange(e) {
     const name = e.target.name;
     const value = e.target.value;
     setField({ ...field, [name]: value });
+    validarObjetoDefinido(field)
+    console.log(field,error)
+  }
+
+  function validarObjetoDefinido(obj,error) {
+    for (const key in obj) {
+      if (obj[key] === undefined) {
+        setEnableButton(true && !validarObjeto(error)); // Si encuentra al menos una propiedad indefinida, retorna false
+      }
+    }
+    setEnableButton(false && !validarObjeto(error)); // Si todas las propiedades están definidas, retorna true
+  }
+
+  function validarObjeto(obj) {
+    if(obj){
+      return Object.values(obj).every(value => value === undefined || value === "" || value === null);
+    }else{
+      return false
+    }
+    
   }
 
   useEffect(() => {
-    console.log(error);
+    if(field){
+      setError(
+        {
+          email: getErrorMessageForField('email',field.email),
+          name: getErrorMessageForField('name',field.name),
+          age: getErrorMessageForField('age',field.age),
+          password: getErrorMessageForField('password',field.password),
+          passwordCheck: getErrorMessageForField('passwordCheck',field.password, field.passwordCheck)
+        }
+      )
+    }
+  }, [field]);
 
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    if (field.email === "") setError({ ...error, email: "email is required" });
-    else if (!emailRegex.test(field.email))
-      setError({ ...error, email: "email is invalid" });
-    // else setError({ ...error, email: "" });
-    else if (field.name === "")
-      setError({ ...error, name: "name is required" });
-    // else setError({ ...error, name: "" });
-    else if (field.age.length === 0)
-      setError({ ...error, age: "age is required" });
-    else if (field.age < 17)
-      setError({ ...error, age: "you must be above 18 to register" });
-    else if (field.password === "")
-      setError({ ...error, password: "password is required" });
-    else if (field.password.length > 5)
-      setError({ ...error, password: "password is too short" });
-    // else setError({ ...error, password: "" });
-    else if (field.passwordCheck !== field.password)
-      setError({ ...error, passwordCheck: "passwords do not match" });
-    else setError({ ...error, password: "" });
-  }, [field.email, field.name, field.age, field.password, field.passwordCheck]);
 
   return (
     <div>
@@ -67,7 +106,7 @@ export function App() {
             <input
               type="email"
               name="email"
-              value={field.email}
+              // value={field.email}
               onChange={(e) => {
                 handleOnChange(e);
               }}
@@ -84,7 +123,7 @@ export function App() {
             <input
               type="text"
               name="name"
-              value={field.name}
+              // value={field.name}
               onChange={(e) => {
                 handleOnChange(e);
               }}
@@ -101,7 +140,7 @@ export function App() {
             <input
               type="number"
               name="age"
-              value={field.age}
+              // value={field.age}
               onChange={(e) => {
                 handleOnChange(e);
               }}
@@ -118,7 +157,7 @@ export function App() {
             <input
               type="password"
               name="password"
-              value={field.password}
+              // value={field.password}
               onChange={(e) => {
                 handleOnChange(e);
               }}
@@ -135,7 +174,7 @@ export function App() {
             <input
               type="password"
               name="passwordCheck"
-              value={field.passwordCheck}
+              // value={field.passwordCheck}
               onChange={(e) => {
                 handleOnChange(e);
               }}
@@ -161,7 +200,7 @@ export function App() {
           </span>
         </div>
 
-        <button onSubmit={handleOnSubmit} disabled={true}>
+        <button onSubmit={onSubmit} disabled={enableButton}>
           Sign up
         </button>
       </form>
